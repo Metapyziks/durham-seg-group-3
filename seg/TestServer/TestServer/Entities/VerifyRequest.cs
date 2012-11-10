7bc71e3eeb6e9ddbbecb92ebf613e96bf0bfce6b
James King <metapyziks@gmail.com>
2012-11-10 18:25:47 +0000

    Moved Account and VerifyRequest to TestServer.Entities

--- seg/TestServer/TestServer/Entities/VerifyRequest.cs
+++ seg/TestServer/TestServer/Entities/VerifyRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestServer
+namespace TestServer.Entities
 {
     [DatabaseEntity( "VerifyRequest", VerifyRequest.Fields.VerifyRequestID,
         VerifyRequest.Fields.VerifyRequestID,
